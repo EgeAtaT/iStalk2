04d9c41f8b2fbb2441692aabbc2026b7111cf120
dorukkarinca <dorukkarinca@gmail.com>
2015-06-30 23:45:21 +0300

    More random stuff.

--- app/src/main/java/com/nig/fag/istalk/AllStalksActivity.java
+++ app/src/main/java/com/nig/fag/istalk/AllStalksActivity.java
@@ -1,10 +1,15 @@
 package com.nig.fag.istalk;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 /**
  * Created by Doruk on 30.06.2015.
  */
 public class AllStalksActivity extends Activity {
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_allstalks);
+    }
 }
--- app/src/main/java/com/nig/fag/istalk/ListsActivity.java
+++ app/src/main/java/com/nig/fag/istalk/ListsActivity.java
@@ -1,7 +1,15 @@
 package com.nig.fag.istalk;
 
+import android.app.Activity;
+import android.os.Bundle;
+
 /**
  * Created by Doruk on 30.06.2015.
  */
-public class ListsActivity {
+public class ListsActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lists);
+    }
 }
