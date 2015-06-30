5b9b03ec24da67e75b043aeb2960f28daafff00f
dorukkarinca <dorukkarinca@gmail.com>
2015-06-30 23:32:20 +0300

    Random stuff.

---
+++ app/src/main/java/com/nig/fag/istalk/AllStalksActivity.java
@@ -0,0 +1,7 @@
+package com.nig.fag.istalk;
+
+/**
+ * Created by Doruk on 30.06.2015.
+ */
+public class AllStalksActivity {
+}
--- app/src/main/java/com/nig/fag/istalk/MainActivity.java
+++ app/src/main/java/com/nig/fag/istalk/MainActivity.java
@@ -1,12 +1,12 @@
 package com.nig.fag.istalk;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +29,7 @@ public class MainActivity extends ActionBarActivity {
 
         // TODO: ADD ACTUAL BUTTON FUNCTIONS
         switch (item.getItemId()) {
-            case R.id.action_search:
+            case R.id.action_people:
                 //openSearch();
                 return true;
             case R.id.action_settings:
