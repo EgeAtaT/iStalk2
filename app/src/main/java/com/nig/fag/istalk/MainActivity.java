d09c162431787ec8ef3fe7a89f1cedc561376aed
dorukkarinca <dorukkarinca@gmail.com>
2015-07-01 03:47:19 +0300

    It's 3.46 AM and what the hell. But I'm making progress, which is nice.

--- app/src/main/java/com/nig/fag/istalk/MainActivity.java
+++ app/src/main/java/com/nig/fag/istalk/MainActivity.java
@@ -1,62 +1,14 @@
 package com.nig.fag.istalk;
 
-import android.content.Intent;
-import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentActivity;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.view.Menu;
-import android.view.MenuItem;
 
 
-public class MainActivity extends FragmentActivity {
+public class MainActivity extends SingleFragmentActivity {
 
-    public Intent intent;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        FragmentManager fm = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fm.beginTransaction();
-
-        Fragment fragment = new Fragment();
-        fragmentTransaction.add(R.id.feed, fragment);
-        fragmentTransaction.commit();
+    protected Fragment createFragment(){
+        return new MainFragment();
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main_activity_actions, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-
-        // TODO: ADD ACTUAL BUTTON FUNCTIONS
-        switch (item.getItemId()) {
-            case R.id.action_people:
-                //openSearch();
-                intent = new Intent(this, AllStalksActivity.class);
-                startActivity(intent);
-                return true;
-            case R.id.action_settings:
-                //openSettings();
-                return true;
-            case R.id.action_lists:
-                intent = new Intent(this, ListsActivity.class);
-                startActivity(intent);
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-
-    }
 }
--- app/src/main/java/com/nig/fag/istalk/MainFragment.java
+++ app/src/main/java/com/nig/fag/istalk/MainFragment.java
@@ -1,10 +1,72 @@
 package com.nig.fag.istalk;
 
+import android.content.Intent;
+import android.os.Bundle;
 import android.support.v4.app.ListFragment;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Doruk on 01.07.2015.
  */
 public class MainFragment extends ListFragment {
+    public Intent intent;
+    private List<String> listValues;
 
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        listValues = new ArrayList<String>();
+        listValues.add("Android");
+        listValues.add("iOS");
+        listValues.add("Symbian");
+        listValues.add("Blackberry");
+        listValues.add("Windows Phone");
+        listValues.add("asdf");
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter <String>(getActivity(),
+                R.layout.person_row, R.id.listText, listValues);
+        setListAdapter(myAdapter);
+
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.main_activity_actions, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+
+        // TODO: ADD ACTUAL BUTTON FUNCTIONS
+        switch (item.getItemId()) {
+            case R.id.action_people:
+                //openSearch();
+                intent = new Intent(getActivity(), AllStalksActivity.class);
+                startActivity(intent);
+                return true;
+            case R.id.action_settings:
+                //openSettings();
+                return true;
+            case R.id.action_lists:
+                intent = new Intent(getActivity(), ListsActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
+    }
 }
